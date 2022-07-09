d1ff50786912d91a95db38dabaff6c11dd3ca4a7
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-07-09 14:35:10 +0000

    add basic set up for product page urls, views, template.

--- products/models.py
+++ products/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 
 class Category(models.Model):
+    """
+    baisc modle for product cetegories
+    """
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=250)
     friendly_name = models.CharField(max_length=250, null=True, blank=True)
 
@@ -13,6 +19,9 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    """
+    baisc modle for products
+    """
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=250)
     description = models.TextField()
