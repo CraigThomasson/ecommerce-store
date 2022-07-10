6ca774848060d8866d10892de2611a1b7df48908
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-07-10 23:14:09 +0000

    add search functionality to product view

--- products/models.py
+++ products/models.py
@@ -22,13 +22,18 @@ class Product(models.Model):
     """
     baisc modle for products
     """
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL
+    )
     name = models.CharField(max_length=250)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True
+    )
     image = models.ImageField(null=True, blank=True, default='placeholder')
+    colour = models.CharField(max_length=100, default='white')
+    scent = models.CharField(max_length=100, default='unscented')
 
     def __str__(self):
         return self.name
-    
--- products/views.py
+++ products/views.py
@@ -1,16 +1,38 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
+
 
 def all_products(request):
     """
     This view displays all products and
     handles sorting and searching quries
     """
-    
+
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        query = request.GET['q']
+        if not query:
+            messages.error(request, 'You submitted a blank search')
+            return redirect(reverse('products'))
+
+        queries = Q(
+            name__icontains=query
+        ) | Q(
+            description__icontains=query
+        ) | Q(
+            scent__icontains=query
+        ) | Q(
+            colour__icontains=query
+        )
+        products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_results': query,
     }
 
     return render(request, 'products/products.html', context)
@@ -20,7 +42,7 @@ def view_product(request, product_id):
     """
     This view displays  product detials for selected product
     """
-    
+
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
