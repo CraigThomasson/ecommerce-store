9e383b1835bd537602054e359719ae9bc11db291
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-07-16 21:55:48 +0000

    added session bag to context.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,14 +1,29 @@
 from decimal import Decimal
 from django.conf import settings
+from products.models import Product
+from django.shortcuts import get_object_or_404
+
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
     grand_total = delivery + total
-    
+
     context = {
         'bag_items': bag_items,
         'total': total,
--- bag/views.py
+++ bag/views.py
@@ -13,7 +13,7 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity')) 
     redirect_url = request.POST.get('redirect_url')  # gets request.path from hidden file in product details view
-    bag = request.session.get('bag', {}) # access the saved session in the browser to get the saved session
+    bag = request.session.get('bag', {})  # access the saved session in the browser to get the saved session
 
     # check if item is already in list and ads quantitsy to current item, and item if not in list
     if item_id in list(bag.keys()):
@@ -21,6 +21,5 @@ def add_to_bag(request, item_id):
     else:
         bag[item_id] = quantity
 
-    request.session['bag'] = bag #updates the session bag
-    print(request.session['bag'])
+    request.session['bag'] = bag  # updates the session bag
     return redirect(redirect_url)
