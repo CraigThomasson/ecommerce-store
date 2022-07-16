617b201f763eb0844a5f33ca97b028631240a638
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-07-16 19:29:56 +0000

    add add ot bag view to store shopping bag in the session

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def bag_view(request):
@@ -6,3 +6,21 @@ def bag_view(request):
     veiw from bag page
     """
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity')) 
+    redirect_url = request.POST.get('redirect_url')  # gets request.path from hidden file in product details view
+    bag = request.session.get('bag', {}) # access the saved session in the browser to get the saved session
+
+    # check if item is already in list and ads quantitsy to current item, and item if not in list
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag #updates the session bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
