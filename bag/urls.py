e50d7c4bed6259126a700528c73b60d294a73393
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-07-17 20:34:18 +0000

    added views, js, url and udated bag.html so user can remove items from bag

--- bag/urls.py
+++ bag/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.bag_view, name='bag_view'),
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('update/<item_id>/', views.update_bag, name='update_bag'),
+    path('remove/<item_id>/', views.remove_bag_Item, name='remove_bag_item')
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def bag_view(request):
@@ -11,7 +11,7 @@ def bag_view(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    quantity = int(request.POST.get('quantity')) 
+    quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')  # gets request.path from hidden file in product details view
     bag = request.session.get('bag', {})  # access the saved session in the browser to get the saved session
 
@@ -23,3 +23,34 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag  # updates the session bag
     return redirect(redirect_url)
+
+
+def update_bag(request, item_id):
+    """ update the quantity of the specified product to the shopping bag in the view bag page """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})  # access the saved session in the browser to get the saved session
+
+    # check if item is already in list and ads quantitsy to current item, and item if not in list
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag  # updates the session bag
+    return redirect(reverse('bag_view'))
+
+
+def remove_bag_Item(request, item_id):
+    """ remove item from the bag"""
+
+    try:
+        bag = request.session.get('bag', {})  # access the saved session in the browser to get the saved session
+
+        bag.pop(item_id)
+
+        request.session['bag'] = bag  # updates the session bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
