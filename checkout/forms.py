4c6ccfe782691887dc9af9792aad37db43984916
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-07-20 20:24:45 +0000

    add template and urls for checkout

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from .models import Order
 
-class order_Form(forms.ModelForm):
+class Order_form(forms.ModelForm):
     class Meta:
         model = Order
         fields = (
             'full_name', 'email', 'phone_number',
             'town_or_city', 'postcode', 'country',
-            'county,'
+            'county',
         )
