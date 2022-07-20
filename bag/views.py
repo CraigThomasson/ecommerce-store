8b4c2e730d533970bfd59951e4b7c21cb9311a81
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-07-20 15:00:05 +0000

    add styling to toast and 4 levels of messages

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from products.models import Product
+from django.contrib import messages
 
 
 def bag_view(request):
@@ -11,6 +13,7 @@ def bag_view(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')  # gets request.path from hidden file in product details view
     bag = request.session.get('bag', {})  # access the saved session in the browser to get the saved session
@@ -18,8 +21,10 @@ def add_to_bag(request, item_id):
     # check if item is already in list and ads quantitsy to current item, and item if not in list
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'{product.name} was added to bag')
     else:
         bag[item_id] = quantity
+        messages.success(request, f'{product.name} was added to bag')
 
     request.session['bag'] = bag  # updates the session bag
     return redirect(redirect_url)
