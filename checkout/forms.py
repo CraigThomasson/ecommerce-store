6b860f44f8cd2228c87acc03d72f3d82f4b24e52
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-07-22 16:16:14 +0000

    add checkout_sussess page and add logic to check out view to save form info id user wants to save

--- checkout/forms.py
+++ checkout/forms.py
@@ -6,6 +6,7 @@ class Order_form(forms.ModelForm):
         model = Order
         fields = (
             'full_name', 'email', 'phone_number',
+            'street_address1', 'street_address2',
             'town_or_city', 'postcode', 'country',
             'county',
         )
