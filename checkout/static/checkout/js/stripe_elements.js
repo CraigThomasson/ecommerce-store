b4bf0ef533a2524d246e7eb1c18addb341a6d2cd
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-07-22 13:48:22 +0000

    add core strip payment functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -21,7 +21,7 @@ var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
 
-// Handle realtime validation errors on the card element 
+// Handle realtime validation errors on the card element - code from boutique ado
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
@@ -35,4 +35,35 @@ card.addEventListener('change', function (event) {
     } else {
         errorDiv.textContent = '';
     }
+});
+
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
 });
--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,41 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from . forms import Order_form
-
+from bag.contexts import bag_contents
+import stripe
 
 def checkout(request):
+    """
+    this view deals with the checkout pahe and stripe payment
+    """
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, 'there are no items in your bag.')
         return redirect(reverse('products'))
 
+    # get the current bad from bag.contexts 
+    # and create total to be passes to stripe
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+
     order_form = Order_form()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51L7JpvGht2uOQOxeJoUw7NnqCGEKIAAGdZ0IBlko8VhETTVzgKqmahz7sjycIAs5XU1GVCfeNlkMmMVHBZUNt1Xy00EH5EwnE1',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
