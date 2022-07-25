7c18511eb7ec40e22ab95111adb3de4e33e23bad
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-07-25 13:53:22 +0000

    add about page views, urls, templates and content

--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def index(request):
     veiw from home page
     """
     return render(request, 'home/index.html')
+
+def about(request):
+    """
+    veiw from home page
+    """
+    return render(request, 'home/about.html')
